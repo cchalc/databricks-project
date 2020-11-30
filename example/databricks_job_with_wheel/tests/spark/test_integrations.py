cc63d0259e40a83bbd388b26e9a8a155fa55daa4
Joshua Cook <joshua.cook@koolHerc.lan>
2020-11-29 17:49:02 -0800

    move src code to package `pipelines`

--- example/databricks_job_with_wheel/tests/spark/test_integrations.py
+++ example/databricks_job_with_wheel/tests/spark/test_integrations.py
@@ -4,9 +4,9 @@ from shutil import rmtree
 from pyspark.sql import DataFrame, SparkSession
 from pyspark.sql.types import StructType
 
-from src.config import generate_paths, generate_schemas
-from src.operations import create_stream_writer, transform_raw
-from src.utility import (
+from pipelines.config import generate_paths, generate_schemas
+from pipelines.operations import create_stream_writer, transform_raw, transform_bronze
+from pipelines.utility import (
     generate_spark_session,
     load_delta_table,
     read_stream_json,
@@ -22,6 +22,12 @@ def env() -> str:
 @pytest.fixture(scope="module")
 def spark() -> SparkSession:
     yield generate_spark_session()
+
+
+@pytest.fixture()
+def bronze_df(spark: SparkSession, env: str, bronze_schema: StructType) -> DataFrame:
+    bronze_path = generate_paths(env, "test_bronze")
+    yield read_stream_json(spark, bronze_path, bronze_schema)
 
 
 @pytest.fixture(scope="module")
@@ -40,13 +46,37 @@ def bronze_path(env: str) -> StructType:
 
 @pytest.fixture(scope="module")
 def bronze_schema() -> StructType:
-    yield generate_schemas("BRONZE_SCHEMA")
+    yield generate_schemas("bronze_schema")
+
+
+@pytest.fixture(scope="module")
+def raw_schema() -> StructType:
+    yield generate_schemas("raw_schema")
 
 
 @pytest.fixture()
-def raw_df(spark: SparkSession, env: str) -> DataFrame:
+def raw_df(spark: SparkSession, env: str, raw_schema: StructType) -> DataFrame:
     raw_path = generate_paths(env, "test_raw")
-    yield read_stream_json(spark, raw_path)
+    yield read_stream_json(spark, raw_path, raw_schema)
+
+
+@pytest.fixture(scope="module")
+def silver_checkpoint(env: str) -> StructType:
+    path = generate_paths(env, "silver_checkpoint")
+    yield path
+    rmtree(path)
+
+
+@pytest.fixture(scope="module")
+def silver_path(env: str) -> StructType:
+    path = generate_paths(env, "silver")
+    yield path
+    rmtree(path)
+
+
+@pytest.fixture(scope="module")
+def silver_schema() -> StructType:
+    yield generate_schemas("silver_schema")
 
 
 class TestSparkIntegrations:
@@ -57,11 +87,29 @@ class TestSparkIntegrations:
         transformed_raw_df = transform_raw(spark, raw_df)
         raw_to_bronze_writer = create_stream_writer(
             dataframe=transformed_raw_df,
+            path=bronze_path,
             checkpoint=bronze_checkpoint,
             name=stream_name,
             partition_column="p_ingestdate",
         )
-        raw_to_bronze_writer.start(bronze_path)
+        raw_to_bronze_writer.start()
 
         until_stream_is_ready(spark, stream_name)
         assert load_delta_table(spark, bronze_path).count() == 7320
+
+    def test_bronze_to_silver(
+        self, env, spark, bronze_df, silver_checkpoint, silver_path, silver_schema
+    ):
+        stream_name = "write_bronze_to_silver"
+        transformed_bronze_df = transform_bronze(spark, bronze_df)
+        bronze_to_silver_writer = create_stream_writer(
+            dataframe=transformed_bronze_df,
+            path=silver_path,
+            checkpoint=silver_checkpoint,
+            name=stream_name,
+            partition_column="p_eventdate",
+        )
+        bronze_to_silver_writer.start()
+
+        until_stream_is_ready(spark, stream_name)
+        assert load_delta_table(spark, silver_path).count() == 7320
